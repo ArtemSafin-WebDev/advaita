97f9859d090eecdced2f1bb94d5e9ab31c55190e
Artem Safin <artemsafin.webdev@gmail.com>
2023-01-25 17:55:38 +0300

    Страница не найдена

---
+++ pages-data/not-found.js
@@ -0,0 +1,9 @@
+const notFound = {
+  "/not-found.html": {
+    title: "Страница не найдена",
+    noFooter: true,
+    noHeader: true,
+  },
+};
+
+export default notFound;
--- pages.config.js
+++ pages.config.js
@@ -1,9 +1,11 @@
 import home from "./pages-data/home";
 import contacts from "./pages-data/contacts";
+import notFound from "./pages-data/not-found";
 
 const pagesConfig = {
   ...home,
   ...contacts,
+  ...notFound,
 };
 
 export default pagesConfig;
